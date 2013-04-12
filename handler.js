c66336be240d3dbf03d6c42236336d3469611652
Asheesh Laroia <asheesh@asheesh.org>
2013-04-12 19:28:52 -0400

    use data

--- handler.js
+++ handler.js
@@ -1,8 +1,22 @@
 var handle = function(e) {
-    console.log(e);
+    // Find the relevant link
+    var targetLink = e.currentTarget.href;
+    console.log(targetLink);
+
+    // Find what we said about it
+    var relevantItem = example_items.filter(function(item) { return item['url'] == targetLink});
+    console.log(relevantItem);
+    var desc = relevantItem[0]['Description'];
+    var html = "";
+
+    if (desc) {
+	html = '<p>Notes by event organizers:</p><blockquote>' + desc + '</blockquote>';
+    }
+
+    html = html + '<p style="text-align: right;"><a class="deep_go" href="' + targetLink + '">Go</a>';
 
     var $dialog = $('<div></div>')
-	.html('This dialog will show every time!')
+	.html(html)
 	.dialog({
 	    autoOpen: false,
 	    title: 'Info about this task'
