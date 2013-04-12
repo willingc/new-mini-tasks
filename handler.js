20a4b5e54958d5d35fd7f1d886197f1e7738fc41
Asheesh Laroia <asheesh@asheesh.org>
2013-04-12 19:04:28 -0400

    wip

--- handler.js
+++ handler.js
@@ -1,3 +1,14 @@
-var handle = function() {
-    alert("hi");
+var handle = function(e) {
+    console.log(e);
+
+    var $dialog = $('<div></div>')
+	.html('This dialog will show every time!')
+	.dialog({
+	    autoOpen: false,
+	    title: 'Info about this task'
+	});
+
+    $dialog.dialog('open');
+    // prevent the default action, e.g., following a link
+    return false;
 };
