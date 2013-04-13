1bc0260af698e2e86039c75ed0eac60c8cafae1b
Nathan Yergler <nathan@yergler.net>
2013-04-13 16:29:44 -0700

    Task, Student models.

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+MODE_CHOICES = (
+    ('group', 'group'),
+    ('individual', 'individual'),
+)
+
+
+class Task(models.Model):
+    project = models.CharField(max_length=100)
+    url = models.URLField()
+    skills = models.CharField(max_length=255)
+    community_tools = models.CharField(max_length=255)
+    project_area = models.CharField(max_length=255)
+    summary = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    suggested_for = models.CharField(max_length=255)
+    closed = models.BooleanField(default=False)
+    suggested_mode = models.CharField(
+        max_length=25,
+        choices=MODE_CHOICES,
+    )
+    recently_verified = models.BooleanField(default=False)
+
+
+class Student(models.Model):
+
+    name = models.CharField(max_length=255)
+    task = models.ForeignKey(Task)
